f9fe445cd3e12d8935f7acc2730437a693cb3507
mfp0610 <3346286054@qq.com>
2021-04-15 06:56:33 +0800

    finish question 198 213

---
+++ algorithm/#0198/code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+        if(nums.size()==2) return max(nums[0],nums[1]);
+        int rec,pre1,pre2;
+        pre2=0, pre1=nums[0];
+        for(int i=1;i<nums.size();i++) {
+            rec=max(pre1,pre2+nums[i]);
+            pre2=pre1, pre1=rec;
+        }
+        return rec;
+    }
+};
+//https://leetcode-cn.com/problems/house-robber/ 
